63115ba939e24cf49b3291b78f4ebe85d8709b52
getvkey <ucyour@live.com>
2019-12-09 14:06:07 +0800

    first composer package

---
+++ Flower/Lily.php
@@ -0,0 +1,14 @@
+<?php
+namespace Flower\Lily;
+
+/**
+ * Class Lily
+ * @package Flower\Lily
+ */
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ Flower/Rose.php
@@ -0,0 +1,14 @@
+<?php
+namespace Flower\Rose;
+
+/**
+ * Class Rose
+ * @package Flower\Rose
+ */
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
